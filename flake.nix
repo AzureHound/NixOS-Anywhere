5e48ad1a33c7f6a8c68f3e6d3ee8fe0d18d25f35
Eden <azurehound.landline090@slmail.me>
2026-06-21 13:34:47 +0530

    Init

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "Notebook Init flake";
+
+  inputs = {
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, disko, ... }: {
+    nixosConfigurations.Notebook = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        disko.nixosModules.disko
+
+        ./disko.nix
+        ./hardware.nix
+
+        ({ pkgs, ... }: {
+          boot.loader = {
+            efi.canTouchEfiVariables = true;
+            systemd-boot = {
+              enable = true;
+              consoleMode = "max";
+            };
+          };
+
+          networking = {
+            hostName = "Notebook";
+            networkmanager.enable = true;
+          };
+
+          nix = {
+            settings.experimental-features = [
+              "nix-command"
+              "flakes"
+            ];
+
+            channel.enable = false;
+          };
+
+          services.openssh.enable = true;
+
+          environment.systemPackages = with pkgs; [
+            git
+            vim
+          ];
+
+          users.users.eden = {
+            isNormalUser = true;
+            initialPassword = "Snowflake";
+            extraGroups = [
+              "networkmanager"
+              "wheel"
+            ];
+            openssh.authorizedKeys.keys = [
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPyoX1ZlGb9FFtu9Xw8GE8C+GkwExi03xHZ0LSPnD6zw"
+            ];
+          };
+
+          system.stateVersion = "26.05";
+        })
+      ];
+    };
+  };
+}
